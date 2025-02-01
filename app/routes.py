5fce761c533596b5f8544a665f551cb50155f851
JoeLoparco <loparcojoseph@gmail.com>
2025-02-01 11:18:59 -0600

    Second Commit, New Stuff. Mostly just adding routes to new pages and more info.

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,10 @@ def home(): # Def home function for home page
     return render_template('index.html') # Returns Asscoaited HTML File using flask library render_template
 @appInstance.route('/miniJavaCompiler')
 def compilerProject(): # Creates Route and route function for miniJavaCompiler * NOte Route def must be immiditely after route initialization.
-    return render_template('project.html')
+    return render_template('miniJavaCompiler.html')
+@appInstance.route('/embeddedXinuOS')
+def osProject(): # Creates Route and route function for miniJavaCompiler * NOte Route def must be immiditely after route initialization.
+    return render_template('xinuOS.html')
+@appInstance.route('/rustPythonJIT')
+def rustPyJitComp(): # Creates Route and route function for miniJavaCompiler * NOte Route def must be immiditely after route initialization.
+    return render_template('rustPyJit.html')
